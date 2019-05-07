0024d918821191cfa60daf52fa22ebf3eb967761
zachgw@gmail.com <zachgw@Zacharys-MacBook-Pro.local>
2019-05-06 19:45:26 -0700

    added get todays tide function

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-'use strict';
+//'use strict';
 
-function getTime() {
+/*function convertDate() {
     const today = new Date();
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let month = new Array();
+        month[0] = '01';
+        month[1] = '02';
+        month[2] = '03';
+        month[3] = '04';
+        month[4] = '05';
+        month[5] = '06';
+        month[6] = '07';
+        month[7] = '08';
+        month[8] = '09';
+    let day = new Array();
+        day[1] = '01';
+        day[2] = '02';
+        day[3] = '03';
+        day[4] = '04';
+        day[5] = '05';
+        day[6] = '06';
+        day[7] = '07';
+        day[8] = '08';
+        day[9] = '09';   
+    const date = today.getFullYear()+''+month[today.getMonth()]+''+day[today.getDate()];
     const time = today.getHours() + ":" + today.getMinutes();
     console.log(date, time);
-}
+} */
 
 function getTides(diveDate) {
     fetch(`https://tidesandcurrents.noaa.gov/api/datagetter?begin_date=${diveDate}&range=24&station=9447130&product=predictions&units=english&time_zone=lst_ldt&format=json&datum=mllw&interval=hilo`)
@@ -30,6 +50,7 @@ function displayDiveDateSubmit(diveDate) {
                 <input type="text" name="dateInput" id="dateInput" placeholder="ex. YYYYMMDD" required>
                 <input type="button" id="submitButton" value="go dive">
             </form>
+            <button class="currentDaySlack">Todays Slack</button>
         `);
     });
 }
@@ -41,10 +62,41 @@ function listenForClick() {
     });
 }
 
+function todaySlackClick() {
+    const today = new Date();
+    let month = new Array();
+        month[0] = '01';
+        month[1] = '02';
+        month[2] = '03';
+        month[3] = '04';
+        month[4] = '05';
+        month[5] = '06';
+        month[6] = '07';
+        month[7] = '08';
+        month[8] = '09';
+    let day = new Array();
+        day[1] = '01';
+        day[2] = '02';
+        day[3] = '03';
+        day[4] = '04';
+        day[5] = '05';
+        day[6] = '06';
+        day[7] = '07';
+        day[8] = '08';
+        day[9] = '09';   
+    const date = today.getFullYear()+''+month[today.getMonth()]+''+day[today.getDate()];
+    $('.container').on('click', '.currentDaySlack', function(event) {
+        fetch(`https://tidesandcurrents.noaa.gov/api/datagetter?begin_date=${date}&range=24&station=9447130&product=predictions&units=english&time_zone=lst_ldt&format=json&datum=mllw&interval=hilo`)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+    });
+}
+
 function displayTides(responseJson) {
     console.log(responseJson);
     $('form').remove();
-    $('.time').html(`<p>Time</p>`)
+    $('.time').html(`<p>Time</p>`);
+    $('.returnCity').html(`<button type="submit" class="backToCitiesButton">Choose a different city</button>`);
     responseJson.predictions.forEach(function(item, i) {
         $('.container').append(`
             <div class="tideResponse">
@@ -58,11 +110,26 @@ function displayTides(responseJson) {
     }); 
 }
 
+function backToCities() {
+    $('body').on('click', '.backToCitiesButton', function(event) {
+        $('.container').html(`
+        <div class="cities">
+            <div class="city seattle">Seattle</div>
+            <div class="city tacoma">Tacoma</div>
+            <div class="city portTownsend">Port Townsend</div>
+            <div class="city neahBay">Neah Bay</div>
+        </div>
+        `);
+    });
+}
+
 function runThisPuppy() {
     listenForClick();
     displayDiveDateSubmit();
-    getTime();
     captureCitySelect();
+    backToCities();
+    //convertDate();
+    todaySlackClick();
 };
 
 $(runThisPuppy);
